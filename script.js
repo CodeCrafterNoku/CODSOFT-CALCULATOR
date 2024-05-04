1b079411237703b8b1fc4c3d4b465e0615741997
CodeCrafterNoku <nokubongangema8@gmail.com>
2024-05-04 13:47:06 +0200

    calculator

--- script.js
+++ script.js
@@ -1,20 +1,11 @@
-// Get the input element with the id 'inputBox'
 let input = document.getElementById('inputBox');
-
-// Get all button elements
 let buttons = document.querySelectorAll('button');
-
-// Initialize an empty string to store the input value
 let string = "";
-
 // Convert the buttons NodeList to an array
 let arr = Array.from(buttons);
-
-// Loop through each button element
 arr.forEach(button => {
     // Add a click event listener to each button
     button.addEventListener('click', (e) => {
-        // Check if the clicked button's text content is '='
         if (e.target.innerHTML == '=') {
             // Evaluate the expression in the string and assign the result to string
             string = eval(string);
